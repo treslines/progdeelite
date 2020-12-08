8f575b6f72dadfa7aa2d1abb730d7fa8d47fa1be
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2020-12-08 20:12:52 +0100

    creating dev branch

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
-import { useState } from 'react'
-
 function home(){
    return (
       <div>
-         <h1>Home</h1>
-         <AddContador />
-      </div>
-   )
-}
-
-function AddContador(){
-   const [contador,setContador] = useState(1)
-
-   function contar(){
-      setContador(contador+1)
-   }
-
-   return (
-      <div>
-         <div>{contador}</div>
-         <button onClick={contar}>Contar</button>
+         <h1>Programador de Elite</h1>
       </div>
    )
 }
