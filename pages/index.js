6595b87221d15e86962922491177157a703b72a1
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2020-12-14 20:59:14 +0100

    layouting lading page

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="favicon/favicon.ico" />
       </Head>
 
-      <div class="container my-4 " style={{backgroundColor: '#FFEE00'}}>
+      <div class="container my-4 " /* style={{backgroundColor: '#FFEE00'}} */>
          <div class="row text-center">
             <div class="col-md-6 mb-4">
                <Link href="/_app" >
@@ -36,47 +36,16 @@ export default function Home() {
          </div>
          <div class="row text-center">
             <div class="col-md-6 mb-4">
-               {/* <Link href="https://www.instagram.com/progdeelite/" ><img class="img-responsive" width="46" alt="46x46" src="/insta_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://twitter.com/progdeelite" ><img class="img-responsive" width="46" alt="46x46" src="/twitter_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://web.facebook.com/progdeelite" ><img class="img-responsive" width="46" alt="46x46" src="/face_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link> */}
                <Link href="https://www.youtube.com/channel/UCsEdUK6zNAA95aGlqoBOclQ" ><img class="img-responsive" width="200" alt="46x46" src="/youtube_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
             </div>
          </div>
-      </div>
-
-    </div>
-  )
-}
-
-
-
-/* import Link from 'next/link'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-function Home(){
-   return (
-
-      <div class="container my-4 " style={{backgroundColor: '#FFEE00'}}>
          <div class="row text-center">
             <div class="col-md-6 mb-4">
-               <Link href="/_app" >
-                  <img class="img-responsive" width="250"  alt="100x100" src="/logo.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/>
-               </Link>
-            </div>
-         </div>
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <p class="my-4">VOU TE AJUDAR A CONSEGUIR UM <br></br><span class="font-weight-bold" style={{backgroundColor: ''}}>TOP JOB</span> E SATISFAÇÃO FINANCEIRA!</p>
-               <Link href="https://www.youtube.com/channel/UCsEdUK6zNAA95aGlqoBOclQ" >
-                  <img class="rounded-circle img-responsive" width="100" alt="100x100" src="/perfil.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/>
-               </Link>
-            </div>
-         </div>
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <Link href="https://www.youtube.com/channel/UCsEdUK6zNAA95aGlqoBOclQ" >
-                  <img class="img-responsive" width="250"  alt="100x100" src="/banner_teaser.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/>
-               </Link>
+               <div style={{position: 'relative', width: '100%', paddingBottom: '56.25%'}}>
+                  <iframe allowfullscreen style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', border: '0',}}
+                     src="https://www.youtube.com/embed/3Vp6i47MZys">
+                  </iframe>
+               </div>
             </div>
          </div>
          <div class="row text-center">
@@ -84,12 +53,11 @@ function Home(){
                <Link href="https://www.instagram.com/progdeelite/" ><img class="img-responsive" width="46" alt="46x46" src="/insta_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
                <Link href="https://twitter.com/progdeelite" ><img class="img-responsive" width="46" alt="46x46" src="/twitter_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
                <Link href="https://web.facebook.com/progdeelite" ><img class="img-responsive" width="46" alt="46x46" src="/face_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://www.youtube.com/channel/UCsEdUK6zNAA95aGlqoBOclQ" ><img class="img-responsive" width="56" alt="46x46" src="/youtube_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
+               {/* <Link href="https://www.youtube.com/channel/UCsEdUK6zNAA95aGlqoBOclQ" ><img class="img-responsive" width="200" alt="46x46" src="/youtube_icon.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link> */}
             </div>
          </div>
       </div>
-   )
+
+    </div>
+  )
 }
-
-
-export default Home */
