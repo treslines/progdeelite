58faff26e235f5e0ee44bbdef061322897c9ead6
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-03-05 19:13:12 +0100

    creating components and updating site

--- pages/downloads.js
+++ pages/downloads.js
@@ -1,86 +1,28 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Social from '../src/components/Social'
+import Downloads from '../src/components/Download'
+import Header from '../src/components/Header'
+import Ricardo from '../src/components/Ricardo'
+import Picture from "../src/components/Picture"
+import Like from "../src/components/Like"
 
 export default function Profile() {
   return (
     <div className={styles.container} >
-      <Head>
-        <title>Downloads</title>
-        <link rel="icon" href="favicon/favicon.ico" />
-        <link
-            rel="preload"
-            href="/fonts/Anton-Regular.ttf"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/LuckiestGuy-Regular.ttf"
-            as="font"
-            crossOrigin=""
-          />
-      </Head>
-
+      <Header title="Downloads" />
       <div class="container my-4 " >
-      
          <div class="row text-center">
             <div class="col-md-6 mb-4">
-            <p class="my-4" ><span class="font-weight-normal" style={{backgroundColor: '#FFffff', fontFamily: 'luckiestguy', fontSize: '46px'}}>DOWNLOADS</span></p>
-            <p class="my-4" ><span class="font-weight-normal" style={{backgroundColor: '#FFffff', fontFamily: 'luckiestguy'}}>RICARDO FERREIRA</span><br></br>
-            <Link href="https://tinyurl.com/tprodel" >
-               <b style={{color: 'grey', cursor: 'pointer'}}>@progdeelite</b>
-            </Link>
-            
-            </p>
-               <Link href="https://tinyurl.com/tprodel" >
-                  <img class="rounded-circle img-responsive" width="100" alt="100x100" src="/perfil.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/>
-               </Link>
-               <p class="my-4" style={{}}>
-               VOCÊ É DA <span class="font-weight-normal" style={{backgroundColor: '', fontFamily: 'luckiestguy'}}>
-                  ÁREA DE  TI?</span><br></br>
-                  EU QUERO TE CONHECER!<br></br>
-                  <span class="font-weight-normal" style={{backgroundColor: '#FFEE00', fontFamily: 'luckiestguy'}}>
-                  COMO TE ENCONTRO?</span><br></br>
-               </p>
-               <img class="img-responsive" width="25"  alt="50x50" src="/site_arrow_down.png" data-holder-rendered="true"/>
+               <Ricardo />
+               <Picture destination="tprodel" />
+               <Like />
             </div>
          </div>
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <Link href="https://tinyurl.com/tprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_tiktok.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/iprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_insta.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/rprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_twitter.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/fprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_facebook.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/yprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_youtube.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-            </div>
-         </div>
-         
-         <div class="container" style={{marginTop: '50px'}}>
-            <div class="row">
-               <div class="col-12 col-md-6 center">
-                  <h6 class="text-muted text-center">GRATIS DOWNLOADS</h6> 
-                  <ul class="list-group">
-                     <li class="list-group-item">
-                        <Link href="/docs/CV_modelo_para_enviar_para_empresas_en.docx" >
-                           CV Modelo Europa - Para enviar direto para empresas!
-                        </Link>
-                     </li>
-                     <li class="list-group-item">
-                        <Link href="/docs/CV_modelo_para_iniciantes_em_ingles.docx" >
-                           CV Modelo Iniciante - Para enviar direto para empresas!
-                        </Link>
-                     </li>
-                     <li class="list-group-item">CV Modelo Headhunter - Coming soon! Stay tuned! ;)</li>
-                  </ul>
-               </div>
-            </div>
-         </div>
-         
+         <Social />
+         <Downloads />
       </div>
-
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,120 +1,34 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Logo from '../src/components/Logo'
+import Video from '../src/components/Video'
+import Social from '../src/components/Social'
+import Action from '../src/components/Action'
+import Follow from '../src/components/Follow'
+import Headline from '../src/components/Headline'
+import Header from '../src/components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Home() {
   return (
     <div className={styles.container} >
-      <Head>
-        <title>Prog. De Elite</title>
-        <link rel="icon" href="favicon/favicon.ico" />
-        <link
-            rel="preload"
-            href="/fonts/Anton-Regular.ttf"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/LuckiestGuy-Regular.ttf"
-            as="font"
-            crossOrigin=""
-          />
-      </Head>
-
-      <div class="container my-4 " /* style={{backgroundColor: '#FFEE00'}} */>
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <Link href="/" >
-                  <img class="img-responsive" width="250"  alt="100x100" src="/logo_prog.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/>
-               </Link>
-            </div>
-         </div>
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <Link href="https://tinyurl.com/tprodel" >
-                  <img class="img-responsive" width="250"  alt="100x100" src="/banner_teaser.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/>
-               </Link>
-            </div>
-         </div>
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <p class="my-4" >VOU TE AJUDAR A<br></br> CONSEGUIR UM <span style={{backgroundColor: '#FFEE00', fontFamily: 'luckiestguy'}}>TOP JOB</span>!<br></br><br></br><span style={{fontFamily: 'luckiestguy'}}>ME SEGUE PARA:</span><br></br>OPORTUNIDADES DE EMPREGO,<br></br>
-               TECNOLOGIA, MACETES & TRUQUES!</p>
-               <Link href="/profile" >
-                  <img class="rounded-circle img-responsive" width="100" alt="100x100" src="/perfil.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/>
-               </Link>
-            </div>
-         </div>
-         
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <p class="my-4" style={{}}>
-                  VOCÊ É DA <span class="font-weight-normal" style={{backgroundColor: '', fontFamily: 'luckiestguy'}}>
-                  ÁREA DE  TI?</span><br></br>
-                  EU QUERO TE CONHECER!<br></br>
-                  <span class="font-weight-normal" style={{backgroundColor: '#FFEE00', fontFamily: 'luckiestguy'}}>
-                  COMO TE ENCONTRO?</span><br></br>
-               </p>
-               <img class="img-responsive" width="25"  alt="50x50" src="/site_arrow_down.png" data-holder-rendered="true"/>
-            </div>
-         </div>
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <Link href="https://tinyurl.com/tprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_tiktok.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/iprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_insta.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/rprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_twitter.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/fprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_facebook.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/yprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_youtube.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-            </div>
-         </div>
-         
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <div style={{position: 'relative', width: '100%', paddingBottom: '56.25%'}}>
-                  <iframe allowfullscreen style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', border: '0',}}
-                     src="https://www.youtube.com/embed/3Vp6i47MZys">
-                  </iframe>
-               </div>
-            </div>
-         </div>
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4" >
-               <div style={{position: 'relative', width: '100%', paddingBottom: '56.25%'}}>
-                  <iframe allowfullscreen style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', border: '0',}}
-                     src="https://www.youtube.com/embed/c3wy-9rHECs">
-                  </iframe>
-               </div>
-            </div>
-         </div>
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <div style={{position: 'relative', width: '100%', paddingBottom: '56.25%'}}>
-                  <iframe allowfullscreen style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', border: '0',}}
-                     src="https://www.youtube.com/embed/7sLZc7SCS3M">
-                  </iframe>
-               </div>
-            </div>
-         </div>
-
-         <div class="row text-center">
-            <div class="col-md-6 mb-4">
-               <Link href="https://tinyurl.com/tprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_tiktok.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/iprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_insta.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/rprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_twitter.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/fprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_facebook.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-               <Link href="https://tinyurl.com/yprodel" ><img class="img-responsive" width="46" alt="46x46" src="/site_youtube.png" data-holder-rendered="true" style={{cursor: 'pointer'}}/></Link>
-            </div>
-         </div>
+      <Header title="Prog. De Elite"/>
+      <div class="container my-4 " /* style={{backgroundColor: '#FFEE00'}} */> 
+         <Logo />
+         <Headline />
+         <Follow />
+         <Action />
+         <Social />
+         <Video id="3Vp6i47MZys" />
+         <Video id="c3wy-9rHECs" />
+         <Video id="7sLZc7SCS3M" />
+         <Video id="-gBKLz3sAnY" />
+         <Video id="sA39yl3ZZM0" />
+         <Video id="85Yl1V1RbpY" />
+         <Video id="1fVBePDbfUc" />
+         <Video id="LdW3Nv8Ww7M" />
+         <Video id="iSypMtDIG0k" />
+         <Social />
       </div>
-
     </div>
   )
 }
